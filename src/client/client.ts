27c2fcc119b0005dc34829a3b1aa0d3c0bc84341
OmarMWarraich <earthling986@gmail.com>
2023-04-03 13:01:08 +0500

    feat: Object3D Heirarchy

--- src/client/client.ts
+++ src/client/client.ts
@@ -12,23 +12,49 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 2
+camera.position.x = 4
+camera.position.y = 4
+camera.position.z = 4
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+controls.target.set(8, 0, 0)
 //controls.addEventListener('change', render)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+const light1 = new THREE.PointLight()
+light1.position.set(10, 10, 10)
+scene.add(light1)
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const light2 = new THREE.PointLight()
+light2.position.set(-10, 10, 10)
+scene.add(light2)
+
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    new THREE.MeshPhongMaterial({ color: 0xff0000 })
+)
+object1.position.set(4, 0, 0)
+scene.add(object1)
+object1.add(new THREE.AxesHelper(5))
+
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+)
+object2.position.set(4, 0, 0)
+object1.add(object2)
+object2.add(new THREE.AxesHelper(5))
+
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    new THREE.MeshPhongMaterial({ color: 0x0000ff })
+)
+object3.position.set(4, 0, 0)
+object2.add(object3)
+object3.add(new THREE.AxesHelper(5))
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -38,59 +64,95 @@ function onWindowResize() {
     render()
 }
 
+const gui = new GUI()
+const object1Folder = gui.addFolder('Object1')
+const object1Position = {
+    x: object1.position.x,
+}
+object1Folder.add(object1Position, 'x', 10, 0.01).name('X Position')
+const object1Rotation = {
+    x: object1.rotation.x,
+}
+object1Folder
+    .add(object1Rotation, 'x', 0, Math.PI * 2, 0.01)
+    .name('X Rotation')
+const object1Scale = {
+    x: object1.scale.x,
+}
+object1Folder.add(object1Scale, 'x', 0, 10, 0.01).name('X Scale')
+object1Folder.open()
+
+const object2Folder = gui.addFolder('Object2')
+const object2Position = {
+    x: object2.position.x,
+}
+object2Folder.add(object2Position, 'x', 10, 0.01).name('X Position')
+const object2Rotation = {
+    x: object2.rotation.x,
+}
+object2Folder
+    .add(object2Rotation, 'x', 0, Math.PI * 2, 0.01)
+    .name('X Rotation')
+const object2Scale = {
+    x: object2.scale.x,
+}
+object2Folder.add(object2Scale, 'x', 0, 10, 0.01).name('X Scale')
+object2Folder.open()
+
+const object3Folder = gui.addFolder('Object3')
+const object3Position = {
+    x: object3.position.x,
+}
+object3Folder.add(object3Position, 'x', 10, 0.01).name('X Position')
+const object3Rotation = {
+    x: object3.rotation.x,
+}
+object3Folder
+    .add(object3Rotation, 'x', 0, Math.PI * 2, 0.01)
+    .name('X Rotation')
+const object3Scale = {
+    x: object3.scale.x,
+}
+object3Folder.add(object3Scale, 'x', 0, 10, 0.01).name('X Scale')
+object3Folder.open()
+
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const gui = new GUI()
-const cubeFolder = gui.addFolder('Cube')
-const cubeRotationFolder = cubeFolder.addFolder('Rotation')
-const cubeRotation = {
-    x: cube.rotation.x,
-    y: cube.rotation.y,
-    z: cube.rotation.z,
-}
-cubeRotationFolder.add(cubeRotation, 'x', 0, Math.PI * 2)
-cubeRotationFolder.add(cubeRotation, 'y', 0, Math.PI * 2)
-cubeRotationFolder.add(cubeRotation, 'z', 0, Math.PI * 2)
-cubeFolder.open()
-cubeRotationFolder.open()
-const cubePositionFolder = cubeFolder.addFolder('Position')
-const cubePosition = {
-    x: cube.position.x,
-    y: cube.position.y,
-    z: cube.position.z,
-}
-cubePositionFolder.add(cubePosition, 'x', -10, 10, 2)
-cubePositionFolder.add(cubePosition, 'y', -10, 10, 2)
-cubePositionFolder.add(cubePosition, 'z', -10, 10, 2)
-cubeFolder.open()
-cubePositionFolder.open()
-const cubeScaleFolder = cubeFolder.addFolder('Scale')
-const cubeScale = {
-    x: cube.scale.x,
-    y: cube.scale.y,
-    z: cube.scale.z,
-}
-cubeScaleFolder.add(cubeScale, 'x', -5, 5)
-cubeScaleFolder.add(cubeScale, 'y', -5, 5)
-cubeScaleFolder.add(cubeScale, 'z', -5, 5)
-const cubeVisible = {
-    visible: cube.visible,
-}
-cubeFolder.add(cubeVisible, 'visible')
-cubeFolder.open()
-cubeScaleFolder.open()
+const debug = document.getElementById('debug1') as HTMLDivElement
 
 function animate() {
     requestAnimationFrame(animate)
-
-    //stats.begin()
-    //cube.rotation.x += 0.01
-    //cube.rotation.y += 0.01
-    //stats.end()
-
+    controls.update()
     render()
-
+    const object1WorldPosition = new THREE.Vector3()
+    object1.getWorldPosition(object1WorldPosition)
+    const object2WorldPosition = new THREE.Vector3()
+    object2.getWorldPosition(object2WorldPosition)
+    const object3WorldPosition = new THREE.Vector3()
+    object3.getWorldPosition(object3WorldPosition)
+    debug.innerText =
+        'Red\n' +
+        'Local Pos X : ' +
+        object1.position.x.toFixed(2) +
+        '\n' +
+        'World Pos X : ' +
+        object1WorldPosition.x.toFixed(2) +
+        '\n' +
+        '\nGreen\n' +
+        'Local Pos X : ' +
+        object2.position.x.toFixed(2) +
+        '\n' +
+        'World Pos X : ' +
+        object2WorldPosition.x.toFixed(2) +
+        '\n' +
+        '\nBlue\n' +
+        'Local Pos X : ' +
+        object3.position.x.toFixed(2) +
+        '\n' +
+        'World Pos X : ' +
+        object3WorldPosition.x.toFixed(2) +
+        '\n'
     stats.update()
 }
 
@@ -99,4 +161,3 @@ function render() {
 }
 
 animate()
-//render()
