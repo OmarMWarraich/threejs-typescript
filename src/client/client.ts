03d11fa5bb42d55cf6466c15784267a15d59f9aa
OmarMWarraich <earthling986@gmail.com>
2023-04-03 08:34:24 +0500

    feat: Animation Loop

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,60 +1,51 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from 'three';
 
-const scene = new THREE.Scene()
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10)
-const camera2 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10)
-const camera3 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10)
-const camera4 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10)
+const scene = new THREE.Scene();
 
-camera1.position.z = 2
-camera2.position.y = 1
-camera2.lookAt(new THREE.Vector3(0, 0, 0))
-camera3.position.z = 1
-camera4.position.x = 1
-camera4.lookAt(new THREE.Vector3(0, 0, 0))
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const canvas1 = document.getElementById('c1') as HTMLCanvasElement
-const canvas2 = document.getElementById('c2') as HTMLCanvasElement
-const canvas3 = document.getElementById('c3') as HTMLCanvasElement
-const canvas4 = document.getElementById('c4') as HTMLCanvasElement
-const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 })
-renderer1.setSize(200, 200)
-const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 })
-renderer2.setSize(200, 200)
-const renderer3 = new THREE.WebGLRenderer({ canvas: canvas3 })
-renderer3.setSize(200, 200)
-const renderer4 = new THREE.WebGLRenderer({ canvas: canvas4 })
-renderer4.setSize(200, 200)
+camera.position.z = 2;
 
-//document.body.appendChild(renderer.domElement)
+const renderer = new THREE.WebGLRenderer();
 
-new OrbitControls(camera1, renderer1.domElement)
+renderer.setSize( window.innerWidth, window.innerHeight );
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+document.body.appendChild( renderer.domElement );
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const controls = new OrbitControls(camera, renderer.domElement);
 
-function animate() {
-    requestAnimationFrame(animate)
+controls.addEventListener('change', render); // use if there is no animation loop
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+const geometry = new THREE.BoxGeometry();
 
-    render()
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+
+const cube = new THREE.Mesh( geometry, material );
+
+scene.add( cube );
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // render()
 }
+
+// function animate() {
+//     requestAnimationFrame( animate );
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.01;
+//     render()
+// }
 
 function render() {
-    renderer1.render(scene, camera1)
-    renderer2.render(scene, camera2)
-    renderer3.render(scene, camera3)
-    renderer4.render(scene, camera4)
+    renderer.render( scene, camera );
 }
 
-animate()
+// animate();
+
+render();
