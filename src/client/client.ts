fe089d47cfeee4988dc9911bcbd4541569b03e74
OmarMWarraich <earthling986@gmail.com>
2023-04-04 23:56:34 +0500

    feat: GLTF Model Loader

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,15 +1,14 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.PointLight()
-light.position.set(2.5, 7.5, 15)
-scene.add(light)
+// const light = new THREE.SpotLight();
+// light.position.set(5, 5, 5)
+// scene.add(light);
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -17,70 +16,44 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 3
+camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
+// renderer.physicallyCorrectLights = true //deprecated
+renderer.useLegacyLights = false //use this instead of setting physicallyCorrectLights=true property
+renderer.shadowMap.enabled = true
+// renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
-const mtlLoader = new MTLLoader()
-mtlLoader.load(
-    'models/model2/monkey.mtl',
-    (materials) => {
-        materials.preload()
-
-        const objLoader = new OBJLoader()
-        objLoader.setMaterials(materials)
-        objLoader.load(
-            'models/model2/monkey.obj',
-            (object) => {
-                scene.add(object)
-            },
-            (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-            },
-            (error) => {
-                console.log('An error happened')
+const loader = new GLTFLoader()
+loader.load(
+    'models/model3/monkey.glb',
+    function (gltf) {
+        gltf.scene.traverse(function (child) {
+            if ((child as THREE.Mesh).isMesh) {
+                const m = child as THREE.Mesh
+                m.receiveShadow = true
+                m.castShadow = true
             }
-        )
+            if ((child as THREE.Light).isLight) {
+                const l = child as THREE.Light
+                l.castShadow = true
+                l.shadow.bias = -0.003
+                l.shadow.mapSize.width = 2048
+                l.shadow.mapSize.height = 2048
+            }
+        })
+        scene.add(gltf.scene)
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
     },
     (error) => {
-        console.log('An error happened')
-    }
-)
-
-mtlLoader.load(
-    'models/model2/monkeyTextured.mtl',
-    (materials) => {
-        materials.preload()
-
-        const objLoader = new OBJLoader()
-        objLoader.setMaterials(materials)
-        objLoader.load(
-            'models/model2/monkeyTextured.obj',
-            (object) => {
-                object.position.x = 2.5
-                scene.add(object)
-            },
-            (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-            },
-            (error) => {
-                console.log('An error happened')
-            }
-        )
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log('An error happened')
+        console.log(error)
     }
 )
 
@@ -110,4 +83,3 @@ function render() {
 }
 
 animate()
-
