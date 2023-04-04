f1a9dbe3ceedb80b4cd17cfe66ef78e58331a498
OmarMWarraich <earthling986@gmail.com>
2023-04-05 00:51:31 +0500

    feat: PLY Loader

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,10 +1,14 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
+
+const light = new THREE.SpotLight()
+light.position.set(20, 20, 20)
+scene.add(light)
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -12,12 +16,9 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 2
+camera.position.z = 40
 
 const renderer = new THREE.WebGLRenderer()
-//renderer.physicallyCorrectLights = true //deprecated
-renderer.useLegacyLights = false //use this instead of setting physicallyCorrectLights=true property
-renderer.shadowMap.enabled = true
 renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
@@ -25,25 +26,36 @@ document.body.appendChild(renderer.domElement)
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
-const loader = new GLTFLoader()
+const envTexture = new THREE.CubeTextureLoader().load([
+    'img/px_50.png',
+    'img/nx_50.png',
+    'img/py_50.png',
+    'img/ny_50.png',
+    'img/pz_50.png',
+    'img/nz_50.png'
+])
+envTexture.mapping = THREE.CubeReflectionMapping
+
+const material = new THREE.MeshPhysicalMaterial({
+    color: 0xb2ffc8,
+    envMap: envTexture,
+    metalness: 0,
+    roughness: 0,
+    transparent: true,
+    transmission: 1.0,
+    side: THREE.DoubleSide,
+    clearcoat: 1.0,
+    clearcoatRoughness: 0.25
+})
+
+const loader = new PLYLoader()
 loader.load(
-    'models/model5/monkey_textured.glb',
-    function (gltf) {
-        gltf.scene.traverse(function (child) {
-            if ((child as THREE.Mesh).isMesh) {
-                const m = child as THREE.Mesh
-                m.receiveShadow = true
-                m.castShadow = true
-            }
-            if ((child as THREE.Light).isLight) {
-                const l = child as THREE.Light
-                l.castShadow = true
-                l.shadow.bias = -0.003
-                l.shadow.mapSize.width = 2048
-                l.shadow.mapSize.height = 2048
-            }
-        })
-        scene.add(gltf.scene)
+    'models/sean4.ply',
+    function (geometry) {
+        geometry.computeVertexNormals()
+        const mesh = new THREE.Mesh(geometry, material)
+        mesh.rotateX(-Math.PI / 2)
+        scene.add(mesh)
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
