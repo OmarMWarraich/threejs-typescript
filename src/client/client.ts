abbe6423784c18ef0239ea0e715e8d8cbc43e644
OmarMWarraich <earthling986@gmail.com>
2023-04-04 22:35:38 +0500

    feat: Pointer Lock Controls

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
@@ -11,37 +11,87 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
+camera.position.y = 1
 camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const controls = new TrackballControls(camera, renderer.domElement)
-// controls.addEventListener('change', () => console.log("Controls Change"))
-// controls.addEventListener('start', () => console.log("Controls Start Event"))
-// controls.addEventListener('end', () => console.log("Controls End Event"))
-// controls.enabled = false
-// controls.rotateSpeed = 1.0
-// controls.zoomSpeed = 1.2
-// controls.panSpeed = 0.8
-// controls.keys = ['KeyA', 'KeyS', 'KeyD']
-// controls.noPan = true //default false
-// controls.noRotate = true //default false
-// controls.noZoom = true //default false
-// controls.staticMoving = true //default false
-// controls.dynamicDampingFactor = 0.1
-// controls.maxDistance = 4
-// controls.minDistance = 2
+const menuPanel = document.getElementById('menuPanel') as HTMLDivElement
+const startButton = document.getElementById('startButton') as HTMLInputElement
+startButton.addEventListener(
+    'click',
+    function () {
+        controls.lock()
+    },
+    false
+)
 
-const geometry = new THREE.BoxGeometry()
+const controls = new PointerLockControls(camera, renderer.domElement)
+//controls.addEventListener('change', () => console.log("Controls Change"))
+controls.addEventListener('lock', () => (menuPanel.style.display = 'none'))
+controls.addEventListener('unlock', () => (menuPanel.style.display = 'block'))
+
+const planeGeometry = new THREE.PlaneGeometry(100, 100, 50, 50)
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
+const plane = new THREE.Mesh(planeGeometry, material)
+plane.rotateX(-Math.PI / 2)
+scene.add(plane)
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const cubes: THREE.Mesh[] = []
+for (let i = 0; i < 100; i++) {
+    const geo = new THREE.BoxGeometry(
+        Math.random() * 4,
+        Math.random() * 16,
+        Math.random() * 4
+    )
+    const mat = new THREE.MeshBasicMaterial({ wireframe: true })
+    switch (i % 3) {
+        case 0:
+            mat.color = new THREE.Color(0xff0000)
+            break
+        case 1:
+            mat.color = new THREE.Color(0xffff00)
+            break
+        case 2:
+            mat.color = new THREE.Color(0x0000ff)
+            break
+    }
+    const cube = new THREE.Mesh(geo, mat)
+    cubes.push(cube)
+}
+cubes.forEach((c) => {
+    c.position.x = Math.random() * 100 - 50
+    c.position.z = Math.random() * 100 - 50
+    c.geometry.computeBoundingBox()
+    c.position.y =
+        ((c.geometry.boundingBox as THREE.Box3).max.y -
+            (c.geometry.boundingBox as THREE.Box3).min.y) /
+        2
+    scene.add(c)
+})
+
+const onKeyDown = function (event: KeyboardEvent) {
+    switch (event.code) {
+        case 'KeyW':
+            controls.moveForward(0.25)
+            break
+        case 'KeyA':
+            controls.moveRight(-0.25)
+            break
+        case 'KeyS':
+            controls.moveForward(-0.25)
+            break
+        case 'KeyD':
+            controls.moveRight(0.25)
+            break
+    }
+}
+document.addEventListener('keydown', onKeyDown, false)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -57,8 +107,7 @@ document.body.appendChild(stats.dom)
 function animate() {
     requestAnimationFrame(animate)
 
-    // trackball controls needs to be updated in the animation loop before it will work
-    controls.update()
+    //controls.update()
 
     render()
 
