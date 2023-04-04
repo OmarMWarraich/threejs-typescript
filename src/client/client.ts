9a7307442c51dba23eaf0e9c1fbed69284c40a71
OmarMWarraich <earthling986@gmail.com>
2023-04-04 22:38:02 +0500

    feat: Drag Controls

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,9 +1,13 @@
 import * as THREE from 'three'
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
+import { DragControls } from 'three/examples/jsm/controls/DragControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
+
+const light = new THREE.PointLight()
+light.position.set(10, 10, 10)
+scene.add(light)
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -11,87 +15,40 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 3
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const menuPanel = document.getElementById('menuPanel') as HTMLDivElement
-const startButton = document.getElementById('startButton') as HTMLInputElement
-startButton.addEventListener(
-    'click',
-    function () {
-        controls.lock()
-    },
-    false
-)
+const geometry = new THREE.BoxGeometry()
+//const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000, transparent: true })
+//const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
+//scene.add(cube)
 
-const controls = new PointerLockControls(camera, renderer.domElement)
-//controls.addEventListener('change', () => console.log("Controls Change"))
-controls.addEventListener('lock', () => (menuPanel.style.display = 'none'))
-controls.addEventListener('unlock', () => (menuPanel.style.display = 'block'))
+const material = [
+    new THREE.MeshPhongMaterial({ color: 0xff0000, transparent: true }),
+    new THREE.MeshPhongMaterial({ color: 0x00ff00, transparent: true }),
+    new THREE.MeshPhongMaterial({ color: 0x0000ff, transparent: true })
+]
 
-const planeGeometry = new THREE.PlaneGeometry(100, 100, 50, 50)
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
-const plane = new THREE.Mesh(planeGeometry, material)
-plane.rotateX(-Math.PI / 2)
-scene.add(plane)
+const cubes = [
+    new THREE.Mesh(geometry, material[0]),
+    new THREE.Mesh(geometry, material[1]),
+    new THREE.Mesh(geometry, material[2])
+]
+cubes[0].position.x = -2
+cubes[1].position.x = 0
+cubes[2].position.x = 2
+cubes.forEach((c) => scene.add(c))
 
-const cubes: THREE.Mesh[] = []
-for (let i = 0; i < 100; i++) {
-    const geo = new THREE.BoxGeometry(
-        Math.random() * 4,
-        Math.random() * 16,
-        Math.random() * 4
-    )
-    const mat = new THREE.MeshBasicMaterial({ wireframe: true })
-    switch (i % 3) {
-        case 0:
-            mat.color = new THREE.Color(0xff0000)
-            break
-        case 1:
-            mat.color = new THREE.Color(0xffff00)
-            break
-        case 2:
-            mat.color = new THREE.Color(0x0000ff)
-            break
-    }
-    const cube = new THREE.Mesh(geo, mat)
-    cubes.push(cube)
-}
-cubes.forEach((c) => {
-    c.position.x = Math.random() * 100 - 50
-    c.position.z = Math.random() * 100 - 50
-    c.geometry.computeBoundingBox()
-    c.position.y =
-        ((c.geometry.boundingBox as THREE.Box3).max.y -
-            (c.geometry.boundingBox as THREE.Box3).min.y) /
-        2
-    scene.add(c)
-})
-
-const onKeyDown = function (event: KeyboardEvent) {
-    switch (event.code) {
-        case 'KeyW':
-            controls.moveForward(0.25)
-            break
-        case 'KeyA':
-            controls.moveRight(-0.25)
-            break
-        case 'KeyS':
-            controls.moveForward(-0.25)
-            break
-        case 'KeyD':
-            controls.moveRight(0.25)
-            break
-    }
-}
-document.addEventListener('keydown', onKeyDown, false)
+const controls = new DragControls(cubes, camera, renderer.domElement)
+// controls.addEventListener('dragstart', function (event) {
+//     event.object.material.opacity = 0.33
+// })
+// controls.addEventListener('dragend', function (event) {
+//     event.object.material.opacity = 1
+// })
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -107,6 +64,12 @@ document.body.appendChild(stats.dom)
 function animate() {
     requestAnimationFrame(animate)
 
+    cubes[0].rotation.x += 0.01
+    cubes[0].rotation.y += 0.011
+    cubes[1].rotation.x += 0.012
+    cubes[1].rotation.y += 0.013
+    cubes[2].rotation.x += 0.014
+    cubes[2].rotation.y += 0.015
     //controls.update()
 
     render()
@@ -118,5 +81,5 @@ function render() {
     renderer.render(scene, camera)
 }
 
-// animate()
 animate()
+
