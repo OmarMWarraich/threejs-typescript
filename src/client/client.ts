27993a22573f43ac132c68391d89c5ddf7a46f48
OmarMWarraich <earthling986@gmail.com>
2023-04-06 02:16:58 +0500

    feat: Collission Detection using OBB

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,38 +1,20 @@
 /* 
 *  Description
 *
-*  Constructive Solid Geometry (CSG) is a modeling technique that uses Boolean
+*  Collision Detection using OBB: Oriented Bounding Box
 *
 */
 
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
-import { CSG } from './utils/CSGMesh'
+import { OBB } from 'three/examples/jsm/math/OBB'
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
-var light1 = new THREE.SpotLight()
-light1.position.set(2.5, 5, 5)
-light1.angle = Math.PI / 4
-light1.penumbra = 0.5
-light1.castShadow = true
-light1.shadow.mapSize.width = 1024
-light1.shadow.mapSize.height = 1024
-light1.shadow.camera.near = 0.5
-light1.shadow.camera.far = 20
-scene.add(light1)
-
-var light2 = new THREE.SpotLight()
-light2.position.set(-2.5, 5, 5)
-light2.angle = Math.PI / 4
-light2.penumbra = 0.5
-light2.castShadow = true
-light2.shadow.mapSize.width = 1024
-light2.shadow.mapSize.height = 1024
-light2.shadow.camera.near = 0.5
-light2.shadow.camera.far = 20
-scene.add(light2)
+const light = new THREE.AmbientLight()
+scene.add(light)
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -40,102 +22,44 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.x = 0.5
-camera.position.y = 2
-camera.position.z = 2.5
+camera.position.set(0.8, 1.4, 3.0)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
 
-{
-    //create a cube and sphere and intersect them
-    const cubeMesh = new THREE.Mesh(
-        new THREE.BoxGeometry(2, 2, 2),
-        new THREE.MeshStandardMaterial({ color: 0xff0000 })
-    )
-    const sphereMesh = new THREE.Mesh(
-        new THREE.SphereGeometry(1.45, 8, 8),
-        new THREE.MeshStandardMaterial({ color: 0x0000ff })
-    )
-    cubeMesh.position.set(-5, 0, -6)
-    scene.add(cubeMesh)
-    sphereMesh.position.set(-2, 0, -6)
-    scene.add(sphereMesh)
+const geometry = new THREE.BoxGeometry(1, 2, 3)
+geometry.computeBoundingBox()
+const material = new THREE.MeshPhongMaterial()
+const mesh = new THREE.Mesh(geometry, material)
+mesh.position.set(4, 1, 0)
+mesh.geometry.userData.obb = new OBB().fromBox3(
+    mesh.geometry.boundingBox as THREE.Box3
+)
+mesh.userData.obb = new OBB()
+scene.add(mesh)
 
-    const cubeCSG = CSG.fromMesh(cubeMesh, 0)
-    const sphereCSG = CSG.fromMesh(sphereMesh, 1)
+const mesh2 = new THREE.Mesh(
+    geometry,
+    new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+)
+mesh2.position.set(-3, 1, 0)
+mesh2.geometry.userData.obb = new OBB().fromBox3(
+    mesh2.geometry.boundingBox as THREE.Box3
+)
+mesh2.userData.obb = new OBB()
 
-    const cubeSphereIntersectCSG = cubeCSG.intersect(sphereCSG)
-    const cubeSphereIntersectMesh = CSG.toMesh(
-        cubeSphereIntersectCSG,
-        new THREE.Matrix4(),
-        [cubeMesh.material, sphereMesh.material]
-    )
-    cubeSphereIntersectMesh.position.set(-2.5, 0, -3)
-    scene.add(cubeSphereIntersectMesh)
+scene.add(mesh2)
 
-    // create 3 cylinders and union them
-    const cylinderMesh1 = new THREE.Mesh(
-        new THREE.CylinderGeometry(0.85, 0.85, 2, 8, 1, false),
-        new THREE.MeshStandardMaterial({
-            color: 0xffbf00,
-        })
-    )
-    const cylinderMesh2 = new THREE.Mesh(
-        new THREE.CylinderGeometry(0.85, 0.85, 2, 8, 1, false),
-        new THREE.MeshStandardMaterial({
-            color: 0x00ff00,
-        })
-    )
-    const cylinderMesh3 = new THREE.Mesh(
-        new THREE.CylinderGeometry(0.85, 0.85, 2, 8, 1, false),
-        new THREE.MeshStandardMaterial({
-            color: 0x9f2b68,
-        })
-    )
-    cylinderMesh1.position.set(1, 0, -6)
-    scene.add(cylinderMesh1)
-    cylinderMesh2.position.set(3, 0, -6)
-    cylinderMesh2.geometry.rotateX(Math.PI / 2)
-    scene.add(cylinderMesh2)
-    cylinderMesh3.position.set(5, 0, -6)
-    cylinderMesh3.geometry.rotateZ(Math.PI / 2)
-    scene.add(cylinderMesh3)
-
-    const cylinderCSG1 = CSG.fromMesh(cylinderMesh1, 2)
-    const cylinderCSG2 = CSG.fromMesh(cylinderMesh2, 3)
-    const cylinderCSG3 = CSG.fromMesh(cylinderMesh3, 4)
-    const cylindersUnionCSG = cylinderCSG1.union(
-        cylinderCSG2.union(cylinderCSG3)
-    )
-
-    const cylindersUnionMesh = CSG.toMesh(
-        cylindersUnionCSG,
-        new THREE.Matrix4()
-    )
-    cylindersUnionMesh.material = [
-        cylinderMesh1.material,
-        cylinderMesh2.material,
-        cylinderMesh3.material,
-    ]
-    cylindersUnionMesh.position.set(2.5, 0, -3)
-    scene.add(cylindersUnionMesh)
-
-    //subtract the cylindersUnionCSG from the cubeSphereIntersectCSG
-    const finalCSG = cubeSphereIntersectCSG.subtract(cylindersUnionCSG)
-    const finalMesh = CSG.toMesh(finalCSG, new THREE.Matrix4())
-    finalMesh.material = [
-        cubeMesh.material,
-        sphereMesh.material,
-        cylinderMesh1.material,
-        cylinderMesh2.material,
-        cylinderMesh3.material,
-    ]
-    scene.add(finalMesh)
-}
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20, 10, 10),
+    new THREE.MeshBasicMaterial({ color: 0xaec6cf, wireframe: true })
+)
+floor.rotateX(-Math.PI / 2)
+scene.add(floor)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -148,10 +72,27 @@ function onWindowResize() {
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
+const clock = new THREE.Clock()
+
 function animate() {
     requestAnimationFrame(animate)
 
+    mesh.position.x = Math.sin(clock.getElapsedTime() * 0.5) * 4
+
     controls.update()
+
+    mesh.userData.obb.copy(mesh.geometry.userData.obb)
+    mesh2.userData.obb.copy(mesh2.geometry.userData.obb)
+    mesh.userData.obb.applyMatrix4(mesh.matrixWorld)
+    mesh2.userData.obb.applyMatrix4(mesh2.matrixWorld)
+    if (mesh.userData.obb.intersectsOBB(mesh2.userData.obb)) {
+        mesh.material.color.set(0xff0000)
+    } else {
+        mesh.material.color.set(0x00ff00)
+    }
+
+    mesh.rotateY(0.01)
+    mesh2.rotateY(-0.005)
 
     render()
 
@@ -163,4 +104,5 @@ function render() {
 }
 
 animate()
+
 
