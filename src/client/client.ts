62701ae1880bd743227564d47e9ffa66df153b7b
OmarMWarraich <earthling986@gmail.com>
2023-04-05 22:28:43 +0500

    feat: Cube Camera Refractions

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,19 +1,31 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x87ceeb)
+scene.background = new THREE.CubeTextureLoader().load([
+    'img/materialTextures2/px_eso0932a.jpg',
+    'img/materialTextures2/nx_eso0932a.jpg',
+    'img/materialTextures2/py_eso0932a.jpg',
+    'img/materialTextures2/ny_eso0932a.jpg',
+    'img/materialTextures2/pz_eso0932a.jpg',
+    'img/materialTextures2/nz_eso0932a.jpg',
+])
 
 const ambientLight = new THREE.AmbientLight(0xaaaaaa)
 scene.add(ambientLight)
 
-const light1 = new THREE.PointLight()
-light1.position.set(10, 10, 10)
+const light1 = new THREE.DirectionalLight()
+light1.position.set(5, 10, 5)
 light1.castShadow = true
 light1.shadow.bias = -0.0002
-light1.shadow.mapSize.height = 2048
-light1.shadow.mapSize.width = 2048
+light1.shadow.mapSize.height = 1024
+light1.shadow.mapSize.width = 1024
+light1.shadow.camera.left = -10
+light1.shadow.camera.right = 10
+light1.shadow.camera.top = 10
+light1.shadow.camera.bottom = -10
 scene.add(light1)
 
 const camera = new THREE.PerspectiveCamera(
@@ -22,7 +34,7 @@ const camera = new THREE.PerspectiveCamera(
     0.01,
     100
 )
-camera.position.set(1.75, 1.75, 3.5)
+camera.position.set(0, 8, 0)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -34,7 +46,7 @@ orbitControls.enableDamping = true
 
 const planeGeometry = new THREE.PlaneGeometry(25, 25)
 const texture = new THREE.TextureLoader().load('img/grid.png')
-const plane = new THREE.Mesh(
+const plane: THREE.Mesh = new THREE.Mesh(
     planeGeometry,
     new THREE.MeshPhongMaterial({ map: texture })
 )
@@ -50,21 +62,39 @@ function onWindowResize() {
     render()
 }
 
-const cubeRenderTarget1 = new THREE.WebGLCubeRenderTarget(128, {
-    generateMipmaps: true,
-    minFilter: THREE.LinearMipmapLinearFilter,
-})
-const cubeRenderTarget2 = new THREE.WebGLCubeRenderTarget(128, {
-    generateMipmaps: true,
-    minFilter: THREE.LinearMipmapLinearFilter,
-})
-const cubeRenderTarget3 = new THREE.WebGLCubeRenderTarget(128, {
-    generateMipmaps: true,
-    minFilter: THREE.LinearMipmapLinearFilter,
-})
-const cubeCamera1 = new THREE.CubeCamera(0.1, 1000, cubeRenderTarget1)
-const cubeCamera2 = new THREE.CubeCamera(0.1, 1000, cubeRenderTarget2)
-const cubeCamera3 = new THREE.CubeCamera(0.1, 1000, cubeRenderTarget3)
+const cubeRenderTarget1: THREE.WebGLCubeRenderTarget =
+    new THREE.WebGLCubeRenderTarget(128, {
+        format: THREE.RGBAFormat,
+        generateMipmaps: true,
+        minFilter: THREE.LinearMipmapLinearFilter,
+    })
+const cubeRenderTarget2: THREE.WebGLCubeRenderTarget =
+    new THREE.WebGLCubeRenderTarget(128, {
+        format: THREE.RGBAFormat,
+        generateMipmaps: true,
+        minFilter: THREE.LinearMipmapLinearFilter,
+    })
+const cubeRenderTarget3: THREE.WebGLCubeRenderTarget =
+    new THREE.WebGLCubeRenderTarget(128, {
+        format: THREE.RGBAFormat,
+        generateMipmaps: true,
+        minFilter: THREE.LinearMipmapLinearFilter,
+    })
+const cubeCamera1: THREE.CubeCamera = new THREE.CubeCamera(
+    0.1,
+    1000,
+    cubeRenderTarget1
+)
+const cubeCamera2: THREE.CubeCamera = new THREE.CubeCamera(
+    0.1,
+    1000,
+    cubeRenderTarget2
+)
+const cubeCamera3: THREE.CubeCamera = new THREE.CubeCamera(
+    0.1,
+    1000,
+    cubeRenderTarget3
+)
 
 const pivot1 = new THREE.Object3D()
 scene.add(pivot1)
@@ -74,14 +104,39 @@ const pivot3 = new THREE.Object3D()
 scene.add(pivot3)
 
 const material1 = new THREE.MeshPhongMaterial({
+    shininess: 100,
+    color: 0xffffff,
+    specular: 0xffffff,
     envMap: cubeRenderTarget1.texture,
+    refractionRatio: 0.5,
+    transparent: true,
+    side: THREE.BackSide,
+    combine: THREE.MixOperation,
 })
 const material2 = new THREE.MeshPhongMaterial({
+    shininess: 100,
+    color: 0xffffff,
+    specular: 0xffffff,
     envMap: cubeRenderTarget2.texture,
+    refractionRatio: 0.5,
+    transparent: true,
+    side: THREE.BackSide,
+    combine: THREE.MixOperation,
 })
 const material3 = new THREE.MeshPhongMaterial({
+    shininess: 100,
+    color: 0xffffff,
+    specular: 0xffffff,
     envMap: cubeRenderTarget3.texture,
+    refractionRatio: 0.5,
+    transparent: true,
+    side: THREE.BackSide,
+    combine: THREE.MixOperation,
 })
+
+cubeRenderTarget1.texture.mapping = THREE.CubeRefractionMapping
+cubeRenderTarget2.texture.mapping = THREE.CubeRefractionMapping
+cubeRenderTarget3.texture.mapping = THREE.CubeRefractionMapping
 
 const ball1 = new THREE.Mesh(new THREE.SphereGeometry(1, 32, 32), material1)
 ball1.position.set(1, 1.1, 0)
@@ -103,6 +158,19 @@ ball3.castShadow = true
 ball3.receiveShadow = true
 ball3.add(cubeCamera3)
 pivot3.add(ball3)
+
+const data = { refractionRatio: 0 }
+
+const gui = new GUI()
+const refractionFolder = gui.addFolder('Refraction')
+refractionFolder
+    .add(data, 'refractionRatio', 0, 1, 0.01)
+    .onChange((v: number) => {
+        material1.refractionRatio = v
+        material2.refractionRatio = v
+        material3.refractionRatio = v
+    })
+refractionFolder.open()
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
@@ -128,12 +196,17 @@ function animate() {
 }
 
 function render() {
+    ball1.visible = false
     cubeCamera1.update(renderer, scene)
+    ball1.visible = true
+    ball2.visible = false
     cubeCamera2.update(renderer, scene)
+    ball2.visible = true
+    ball3.visible = false
     cubeCamera3.update(renderer, scene)
+    ball3.visible = true
 
     renderer.render(scene, camera)
 }
 
 animate()
-
