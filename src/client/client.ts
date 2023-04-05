7ccd0a4cf8e5c1645509b7c98f53af974cb1daf3
OmarMWarraich <earthling986@gmail.com>
2023-04-05 23:22:10 +0500

    feat: Raycaster Measurements

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,19 +1,15 @@
 /* 
-* Description
-* 
-*  This is a basic example of using the Raycaster to mouse pick objects in the scene.
-*  The scene is traversed and all individual objects are added to the pickableObjects 
-*  array that is used by the Raycaster. The sphere and plane are deliberately excluded
-*  from this so they will not be mouse picked.
+*  Description
 *
-*  The plane also receives shadows while everything else only casts shadows.
-* 
+*  Using the Raycaster to assist in measuring the distance between points.
+*
 *  This example demonstrates,
-* 
-*  Loading a GLB scene, traversing the child objects to individually add and/or copy
-*  properties for later use.
-*  Using the Raycaster to detect if the mouse is over certain objects and changing
-*  there material.
+*
+*   Using the Raycaster, along with pointer events to start and draw a line 
+*   from point A to B.
+*
+*   Using the CSS2DRenderer and CSS2DObject to create measurement labels.
+*   Using Vector3.lerpVectors to position a label between 2 points.
 * 
 */
 
@@ -21,9 +17,12 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import {
+    CSS2DRenderer,
+    CSS2DObject,
+} from 'three/examples/jsm/renderers/CSS2DRenderer'
 
 const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
 
 const light = new THREE.SpotLight()
 light.position.set(12.5, 12.5, 12.5)
@@ -46,17 +45,17 @@ renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+const labelRenderer = new CSS2DRenderer()
+labelRenderer.setSize(window.innerWidth, window.innerHeight)
+labelRenderer.domElement.style.position = 'absolute'
+labelRenderer.domElement.style.top = '0px'
+labelRenderer.domElement.style.pointerEvents = 'none'
+document.body.appendChild(labelRenderer.domElement)
+
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
 const pickableObjects: THREE.Mesh[] = []
-let intersectedObject: THREE.Object3D | null
-const originalMaterials: { [id: string]: THREE.Material | THREE.Material[] } =
-    {}
-const highlightedMaterial = new THREE.MeshBasicMaterial({
-    wireframe: true,
-    color: 0x00ff00,
-})
 
 const loader = new GLTFLoader()
 loader.load(
@@ -65,20 +64,14 @@ loader.load(
         gltf.scene.traverse(function (child) {
             if ((child as THREE.Mesh).isMesh) {
                 const m = child as THREE.Mesh
-                //the sphere and plane will not be mouse picked. THe plane will receive shadows while everything else casts shadows.
                 switch (m.name) {
                     case 'Plane':
                         m.receiveShadow = true
                         break
-                    case 'Sphere':
-                        m.castShadow = true
-                        break
                     default:
                         m.castShadow = true
-                        pickableObjects.push(m)
-                        //store reference to original materials for later
-                        originalMaterials[m.name] = (m as THREE.Mesh).material
                 }
+                pickableObjects.push(m)
             }
         })
         scene.add(gltf.scene)
@@ -96,36 +89,130 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
+    labelRenderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
 
+let ctrlDown = false
+let lineId = 0
+let line: THREE.Line
+let drawingLine = false
+const measurementLabels: { [key: number]: CSS2DObject } = {}
+
+window.addEventListener('keydown', function (event) {
+    if (event.key === 'Control') {
+        ctrlDown = true
+        controls.enabled = false
+        renderer.domElement.style.cursor = 'crosshair'
+    }
+})
+
+window.addEventListener('keyup', function (event) {
+    if (event.key === 'Control') {
+        ctrlDown = false
+        controls.enabled = true
+        renderer.domElement.style.cursor = 'pointer'
+        if (drawingLine) {
+            //delete the last line because it wasn't committed
+            scene.remove(line)
+            scene.remove(measurementLabels[lineId])
+            drawingLine = false
+        }
+    }
+})
+
 const raycaster = new THREE.Raycaster()
 let intersects: THREE.Intersection[]
-
 const mouse = new THREE.Vector2()
 
-function onDocumentMouseMove(event: MouseEvent) {
-    mouse.set(
-        (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
-        -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
-    )
-    raycaster.setFromCamera(mouse, camera)
-    intersects = raycaster.intersectObjects(pickableObjects, false)
+renderer.domElement.addEventListener('pointerdown', onClick, false)
+function onClick() {
+    if (ctrlDown) {
+        raycaster.setFromCamera(mouse, camera)
+        intersects = raycaster.intersectObjects(pickableObjects, false)
+        if (intersects.length > 0) {
+            if (!drawingLine) {
+                //start the line
+                const points = []
+                points.push(intersects[0].point)
+                points.push(intersects[0].point.clone())
+                const geometry = new THREE.BufferGeometry().setFromPoints(
+                    points
+                )
+                line = new THREE.LineSegments(
+                    geometry,
+                    new THREE.LineBasicMaterial({
+                        color: 0xffffff,
+                        transparent: true,
+                        opacity: 0.75,
+                        // depthTest: false,
+                        // depthWrite: false
+                    })
+                )
+                line.frustumCulled = false
+                scene.add(line)
 
-    if (intersects.length > 0) {
-        intersectedObject = intersects[0].object
-    } else {
-        intersectedObject = null
-    }
-    pickableObjects.forEach((o: THREE.Mesh, i) => {
-        if (intersectedObject && intersectedObject.name === o.name) {
-            pickableObjects[i].material = highlightedMaterial
-        } else {
-            pickableObjects[i].material = originalMaterials[o.name]
+                const measurementDiv = document.createElement(
+                    'div'
+                ) as HTMLDivElement
+                measurementDiv.className = 'measurementLabel'
+                measurementDiv.innerText = '0.0m'
+                const measurementLabel = new CSS2DObject(measurementDiv)
+                measurementLabel.position.copy(intersects[0].point)
+                measurementLabels[lineId] = measurementLabel
+                scene.add(measurementLabels[lineId])
+                drawingLine = true
+            } else {
+                //finish the line
+                const positions = (
+                    line.geometry.attributes.position as THREE.BufferAttribute
+                ).array as Array<number>
+                positions[3] = intersects[0].point.x
+                positions[4] = intersects[0].point.y
+                positions[5] = intersects[0].point.z
+                line.geometry.attributes.position.needsUpdate = true
+                lineId++
+                drawingLine = false
+            }
         }
-    })
+    }
 }
+
 document.addEventListener('mousemove', onDocumentMouseMove, false)
+function onDocumentMouseMove(event: MouseEvent) {
+    event.preventDefault()
+
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+
+    if (drawingLine) {
+        raycaster.setFromCamera(mouse, camera)
+        intersects = raycaster.intersectObjects(pickableObjects, false)
+        if (intersects.length > 0) {
+            const positions = (
+                line.geometry.attributes.position as THREE.BufferAttribute
+            ).array as Array<number>
+            const v0 = new THREE.Vector3(
+                positions[0],
+                positions[1],
+                positions[2]
+            )
+            const v1 = new THREE.Vector3(
+                intersects[0].point.x,
+                intersects[0].point.y,
+                intersects[0].point.z
+            )
+            positions[3] = intersects[0].point.x
+            positions[4] = intersects[0].point.y
+            positions[5] = intersects[0].point.z
+            line.geometry.attributes.position.needsUpdate = true
+            const distance = v0.distanceTo(v1)
+            measurementLabels[lineId].element.innerText =
+                distance.toFixed(2) + 'm'
+            measurementLabels[lineId].position.lerpVectors(v0, v1, 0.5)
+        }
+    }
+}
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
@@ -141,8 +228,8 @@ function animate() {
 }
 
 function render() {
+    labelRenderer.render(scene, camera)
     renderer.render(scene, camera)
 }
 
 animate()
-
