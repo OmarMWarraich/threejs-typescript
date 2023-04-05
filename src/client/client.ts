d1a1da5343e0cdee73cfe8109fb38bd58af6fdee
OmarMWarraich <earthling986@gmail.com>
2023-04-06 01:17:21 +0500

    feat: Physics w Cannon

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,24 +1,38 @@
 /* 
 *  Description
 *
-*  Sometimes you only need a simple positional transform to occur over time. The tween library works very well, but it could be
-*  over engineering if you don't actually need all the features that it offers.
-*  If you only want to move an object from A to B and, and nothing else then you can use the Vector3 .lerp and .lerpVectors methods.
+*  Animation can also be achieved using a Physics library. Lib called Cannon-es needs to be installed.
+*  Cannon physics library ideal for simulatting rigid bodies. Dont have to use it with three.js. but can be.
+*  We will use it to make objects move/interact in a more realistic way and provide collision detection possibilities.
+*  
+*  Basic Concepts
+* - Rigid Body: A body w shape and other props such as mass and inertia that can be simulated by a physics engine.
+* - Shape: A shape is a volume that can be used to define a rigid body.
+* - Constraint: A 3D body has 6 degrees of freedom, 3 for position and three to describe the rotation vector.
+*               A constraint is a limit on one of these degrees of freedom.
+* - Contact Constraint: A type of constraint to simulate friction and restitution. These are like the faces of an object
+*                       where the constraint is applied.
+* - World: A world is a container for rigid bodies and constraints. It also has a gravity vector and a broadphase.
+* - Broadphase: A broadphase is a method to quickly find all the bodies that might be colliding.
+* - Solver: A solver is a method to compute the forces that should be applied to the bodies to satisfy the constraints. 
 *
-*  (method) Vector3.lerp(v1: THREE.Vector3, alpha: number): THREE.Vector3
-*  (method) Vector3.lerpVectors(v1: THREE.Vector3, v2: THREE.Vector3, alpha: number): THREE.Vector3
+*  Collision Detection
+*  Collision detection is the process of finding pairs of objects that are in contact. Various algorithms can be used to
+*  find these pairs as it is a computationally expensive process.
+*  
+* - Narrowphase: Outright body vs body collision detection. This is the most computationally expensive.
+* - Broadphase: Is a compromise on Narrowphase where various other techniques can be used to improve performance.
+* 
+*  Cannon-es provides serveral options for broadphase detection.
+* - NaiveBroadphase: A simple brute force algorithm. It checks all pairs of bodies. This is the slowest but most accurate.
+* - SAPBroadphase: A sweep and prune algorithm. It sorts the bodies along the x axis and then checks for overlaps.
+* - GridBroadphase: A grid based algorithm. It divides the world into a grid and then checks for overlaps.
 *
-*  eg,
-*  cube.position.lerp(new THREE.Vector3(1, 2, 3), 0.05)
-*  v1 : Is the vector to lerp towards.
-*  alpha : Is the percent distance along the line from the current vector to the v1.
-*  v2 : If using .lerpVectors, then you can set an alternate start vector3 to lerp from rather that the current vector3.
-*  Calling .lerp during an animation loop will appear to mimic a Tween using a TWEEN.Easing.Cubic.Out
-*  Calling .lerpVectors is useful if you want to slide an object along an arbitrary line depending on the alpha value. Amongst other things.
-*  Set alpha to low number such as 0.1, and the vector will appear to lerp more slowly, slowing down as it gets closer to the target vector.
-*  Set alpha to 1.0, and the tween will happen instantly in one render cycle.
-*  Double-click on the floor in the example to see a slower lerp. Then experiment with the alphas to see a faster lerp and slide a second 
-*  cube along a line between the first cube and the starting position.
+* Iterations
+*
+* The solver algorithm is run multiple times per step. This is to improve the stability of the simulation.
+* The number of iterations can be set for each constraint type.
+* The default solver iterations is 10 for contacts and 4 for friction.
 *
 */
 
@@ -26,8 +40,32 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'dat.gui'
+import * as CANNON from 'cannon-es'
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
+
+const light1 = new THREE.SpotLight()
+light1.position.set(2.5, 5, 5)
+light1.angle = Math.PI / 4
+light1.penumbra = 0.5
+light1.castShadow = true
+light1.shadow.mapSize.width = 1024
+light1.shadow.mapSize.height = 1024
+light1.shadow.camera.near = 0.5
+light1.shadow.camera.far = 20
+scene.add(light1)
+
+const light2 = new THREE.SpotLight()
+light2.position.set(-2.5, 5, 5)
+light2.angle = Math.PI / 4
+light2.penumbra = 0.5
+light2.castShadow = true
+light2.shadow.mapSize.width = 1024
+light2.shadow.mapSize.height = 1024
+light2.shadow.camera.near = 0.5
+light2.shadow.camera.far = 20
+scene.add(light2)
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -35,37 +73,122 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.set(1, 2, 5)
+camera.position.set(0, 2, 4)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.shadowMap.enabled = true
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.target.y = 0.5
 
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20, 20, 10, 10),
-    new THREE.MeshBasicMaterial({ color: 0xaec6cf, wireframe: true })
-)
-floor.rotateX(-Math.PI / 2)
-scene.add(floor)
+const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0)
+// world.broadphase = new CANNON.NaiveBroadphase()
+// ;(world.solver as CANNON.GSSolver).iterations = 10
+// world.allowSleep = true
 
-const geometry = new THREE.BoxGeometry()
-//the cube used for .lerp
-const cube1 = new THREE.Mesh(
-    geometry,
-    new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
-)
-cube1.position.y = 0.5
-scene.add(cube1)
+const normalMaterial = new THREE.MeshNormalMaterial()
+const phongMaterial = new THREE.MeshPhongMaterial()
 
-//the cube used for .lerpVectors
-const cube2 = new THREE.Mesh(
-    geometry,
-    new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
-)
-cube2.position.y = 0.5
-scene.add(cube2)
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMesh = new THREE.Mesh(cubeGeometry, normalMaterial)
+cubeMesh.position.x = -3
+cubeMesh.position.y = 3
+cubeMesh.castShadow = true
+scene.add(cubeMesh)
+const cubeShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5))
+const cubeBody = new CANNON.Body({ mass: 1 })
+cubeBody.addShape(cubeShape)
+cubeBody.position.x = cubeMesh.position.x
+cubeBody.position.y = cubeMesh.position.y
+cubeBody.position.z = cubeMesh.position.z
+world.addBody(cubeBody)
+
+const sphereGeometry = new THREE.SphereGeometry()
+const sphereMesh = new THREE.Mesh(sphereGeometry, normalMaterial)
+sphereMesh.position.x = -1
+sphereMesh.position.y = 3
+sphereMesh.castShadow = true
+scene.add(sphereMesh)
+const sphereShape = new CANNON.Sphere(1)
+const sphereBody = new CANNON.Body({ mass: 1 })
+sphereBody.addShape(sphereShape)
+sphereBody.position.x = sphereMesh.position.x
+sphereBody.position.y = sphereMesh.position.y
+sphereBody.position.z = sphereMesh.position.z
+world.addBody(sphereBody)
+
+const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
+const icosahedronMesh = new THREE.Mesh(icosahedronGeometry, normalMaterial)
+icosahedronMesh.position.x = 1
+icosahedronMesh.position.y = 3
+icosahedronMesh.castShadow = true
+scene.add(icosahedronMesh)
+const position = (icosahedronMesh.geometry.attributes.position as THREE.BufferAttribute).array
+const icosahedronPoints: CANNON.Vec3[] = []
+for (let i = 0; i < position.length; i += 3) {
+    icosahedronPoints.push(
+        new CANNON.Vec3(position[i], position[i + 1], position[i + 2])
+    )
+}
+const icosahedronFaces: number[][] = []
+for (let i = 0; i < position.length / 3; i += 3) {
+    icosahedronFaces.push([i, i + 1, i + 2])
+}
+const icosahedronShape = new CANNON.ConvexPolyhedron({
+    vertices: icosahedronPoints,
+    faces: icosahedronFaces,
+})
+const icosahedronBody = new CANNON.Body({ mass: 1 })
+icosahedronBody.addShape(icosahedronShape)
+icosahedronBody.position.x = icosahedronMesh.position.x
+icosahedronBody.position.y = icosahedronMesh.position.y
+icosahedronBody.position.z = icosahedronMesh.position.z
+world.addBody(icosahedronBody)
+
+const torusKnotGeometry = new THREE.TorusKnotGeometry()
+const torusKnotMesh = new THREE.Mesh(torusKnotGeometry, normalMaterial)
+torusKnotMesh.position.x = 4
+torusKnotMesh.position.y = 3
+torusKnotMesh.castShadow = true
+scene.add(torusKnotMesh)
+// position = (torusKnotMesh.geometry.attributes.position as THREE.BufferAttribute).array
+// const torusKnotPoints: CANNON.Vec3[] = []
+// for (let i = 0; i < position.length; i += 3) {
+//     torusKnotPoints.push(new CANNON.Vec3(position[i], position[i + 1], position[i + 2]));
+// }
+// const torusKnotFaces: number[][] = []
+// for (let i = 0; i < position.length / 3; i += 3) {
+//     torusKnotFaces.push([i, i + 1, i + 2])
+// }
+// const torusKnotShape = new CANNON.ConvexPolyhedron(torusKnotPoints, torusKnotFaces)
+const torusKnotShape = CreateTrimesh(torusKnotMesh.geometry)
+const torusKnotBody = new CANNON.Body({ mass: 1 })
+torusKnotBody.addShape(torusKnotShape)
+torusKnotBody.position.x = torusKnotMesh.position.x
+torusKnotBody.position.y = torusKnotMesh.position.y
+torusKnotBody.position.z = torusKnotMesh.position.z
+world.addBody(torusKnotBody)
+
+function CreateTrimesh(geometry: THREE.BufferGeometry) {
+    const vertices = (geometry.attributes.position as THREE.BufferAttribute).array
+    const indices = Object.keys(vertices).map(Number)
+    return new CANNON.Trimesh(vertices as [], indices)
+}
+
+const planeGeometry = new THREE.PlaneGeometry(25, 25)
+const planeMesh = new THREE.Mesh(planeGeometry, phongMaterial)
+planeMesh.rotateX(-Math.PI / 2)
+planeMesh.receiveShadow = true
+scene.add(planeMesh)
+const planeShape = new CANNON.Plane()
+const planeBody = new CANNON.Body({ mass: 0 })
+planeBody.addShape(planeShape)
+planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
+world.addBody(planeBody)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -75,50 +198,83 @@ function onWindowResize() {
     render()
 }
 
-const raycaster = new THREE.Raycaster()
-let v1 = new THREE.Vector3(2, 0.5, 2)
-let v2 = new THREE.Vector3(0, 0.5, 0)
-const mouse = new THREE.Vector2()
-
-function onDoubleClick(event: THREE.Event) {
-    mouse.set(
-        (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
-        -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
-    )
-    raycaster.setFromCamera(mouse, camera)
-
-    const intersects = raycaster.intersectObject(floor, false)
-    if (intersects.length > 0) {
-        v1 = intersects[0].point
-        v1.y += 0.5 //raise it so it appears to sit on grid
-        //console.log(v1)
-    }
-}
-renderer.domElement.addEventListener('dblclick', onDoubleClick, false)
-
 const stats = new Stats()
 document.body.appendChild(stats.dom)
-const data = {
-    lerpAlpha: 0.1,
-    lerpVectorsAlpha: 1.0,
-}
+
 const gui = new GUI()
+const physicsFolder = gui.addFolder('Physics')
 
-const lerpFolder = gui.addFolder('.lerp')
-lerpFolder.add(data, 'lerpAlpha', 0, 1.0, 0.01)
-lerpFolder.open()
+const worldGravity = {
+    x: world.gravity.x,
+    y: world.gravity.y,
+    z: world.gravity.z,
+}
 
-const lerpVectorsFolder = gui.addFolder('.lerpVectors')
-lerpVectorsFolder.add(data, 'lerpVectorsAlpha', 0, 1.0, 0.01)
-lerpVectorsFolder.open()
+physicsFolder.add(worldGravity, 'x', -10.0, 10.0, 0.1)
+physicsFolder.add(worldGravity, 'y', -10.0, 10.0, 0.1)
+physicsFolder.add(worldGravity, 'z', -10.0, 10.0, 0.1)
+physicsFolder.open()
+
+const clock = new THREE.Clock()
+let delta
 
 function animate() {
     requestAnimationFrame(animate)
+
     controls.update()
-    cube1.position.lerp(v1, data.lerpAlpha)
-    cube2.position.lerpVectors(v1, v2, data.lerpVectorsAlpha)
-    controls.target.copy(cube1.position)
+
+    //delta = clock.getDelta()
+    delta = Math.min(clock.getDelta(), 0.1)
+    world.step(delta)
+
+    // Copy coordinates from Cannon to Three.js
+    cubeMesh.position.set(
+        cubeBody.position.x,
+        cubeBody.position.y,
+        cubeBody.position.z
+    )
+    cubeMesh.quaternion.set(
+        cubeBody.quaternion.x,
+        cubeBody.quaternion.y,
+        cubeBody.quaternion.z,
+        cubeBody.quaternion.w
+    )
+    sphereMesh.position.set(
+        sphereBody.position.x,
+        sphereBody.position.y,
+        sphereBody.position.z
+    )
+    sphereMesh.quaternion.set(
+        sphereBody.quaternion.x,
+        sphereBody.quaternion.y,
+        sphereBody.quaternion.z,
+        sphereBody.quaternion.w
+    )
+    icosahedronMesh.position.set(
+        icosahedronBody.position.x,
+        icosahedronBody.position.y,
+        icosahedronBody.position.z
+    )
+    icosahedronMesh.quaternion.set(
+        icosahedronBody.quaternion.x,
+        icosahedronBody.quaternion.y,
+        icosahedronBody.quaternion.z,
+        icosahedronBody.quaternion.w
+    )
+    torusKnotMesh.position.set(
+        torusKnotBody.position.x,
+        torusKnotBody.position.y,
+        torusKnotBody.position.z
+    )
+    torusKnotMesh.quaternion.set(
+        torusKnotBody.quaternion.x,
+        torusKnotBody.quaternion.y,
+        torusKnotBody.quaternion.z,
+        torusKnotBody.quaternion.w
+    )
+
     render()
+
     stats.update()
 }
 
