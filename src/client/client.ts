e39c7e32a06acb42bc1eeb5983a0a62f5faae16d
OmarMWarraich <earthling986@gmail.com>
2023-04-06 02:14:17 +0500

    feat: Constructive Solid Geometry

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,27 +1,37 @@
 /* 
 *  Description
 *
-*  ConvexObjectBreaker
+*  Constructive Solid Geometry (CSG) is a modeling technique that uses Boolean
 *
 */
 
 import * as THREE from 'three'
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
-import * as CANNON from 'cannon-es'
-import CannonUtils from './utils/cannonUtils'
-import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry'
-import { ConvexObjectBreaker } from 'three/examples/jsm/misc/ConvexObjectBreaker'
-import { Reflector } from 'three/examples/jsm/objects/Reflector'
+import { CSG } from './utils/CSGMesh'
 
 const scene = new THREE.Scene()
 
-const light1 = new THREE.DirectionalLight()
-light1.position.set(20, 20, 20)
+var light1 = new THREE.SpotLight()
+light1.position.set(2.5, 5, 5)
+light1.angle = Math.PI / 4
+light1.penumbra = 0.5
+light1.castShadow = true
+light1.shadow.mapSize.width = 1024
+light1.shadow.mapSize.height = 1024
+light1.shadow.camera.near = 0.5
+light1.shadow.camera.far = 20
 scene.add(light1)
 
-const light2 = new THREE.DirectionalLight()
-light2.position.set(-20, 20, 20)
+var light2 = new THREE.SpotLight()
+light2.position.set(-2.5, 5, 5)
+light2.angle = Math.PI / 4
+light2.penumbra = 0.5
+light2.castShadow = true
+light2.shadow.mapSize.width = 1024
+light2.shadow.mapSize.height = 1024
+light2.shadow.camera.near = 0.5
+light2.shadow.camera.far = 20
 scene.add(light2)
 
 const camera = new THREE.PerspectiveCamera(
@@ -30,264 +40,101 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
+camera.position.x = 0.5
+camera.position.y = 2
+camera.position.z = 2.5
 
-const renderer = new THREE.WebGLRenderer({ antialias: true })
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const menuPanel = document.getElementById('menuPanel') as HTMLDivElement
-const startButton = document.getElementById('startButton') as HTMLButtonElement
-startButton.addEventListener(
-    'click',
-    function () {
-        controls.lock()
-    },
-    false
-)
+const controls = new OrbitControls(camera, renderer.domElement)
 
-const controls = new PointerLockControls(camera, renderer.domElement)
-controls.addEventListener('lock', () => (menuPanel.style.display = 'none'))
-controls.addEventListener('unlock', () => (menuPanel.style.display = 'block'))
-
-camera.position.y = 1
-camera.position.z = 2
-
-const onKeyDown = function (event: KeyboardEvent) {
-    switch (event.key) {
-        case 'w':
-            controls.moveForward(0.25)
-            break
-        case 'a':
-            controls.moveRight(-0.25)
-            break
-        case 's':
-            controls.moveForward(-0.25)
-            break
-        case 'd':
-            controls.moveRight(0.25)
-            break
-    }
-}
-document.addEventListener('keydown', onKeyDown, false)
-
-const world = new CANNON.World()
-world.gravity.set(0, -9.82, 0)
-//;(world.solver as CANNON.GSSolver).iterations = 20
-//world.allowSleep = true
-
-const material = new THREE.MeshStandardMaterial({
-    //color: 0xa2ffb8,
-    color: 0xffffff,
-    //reflectivity: 0.15,
-    metalness: 1.0,
-    roughness: 0.25,
-    transparent: true,
-    opacity: 0.75,
-    //transmission: 1.0,
-    side: THREE.DoubleSide,
-    //clearcoat: 1.0,
-    //clearcoatRoughness: 0.35,
-})
-
-const pmremGenerator = new THREE.PMREMGenerator(renderer)
-const envTexture = new THREE.TextureLoader().load(
-    'img/pano-equirectangular.jpg',
-    () => {
-        material.envMap = pmremGenerator.fromEquirectangular(envTexture).texture
-    }
-)
-
-const meshes: { [id: string]: THREE.Mesh } = {}
-const bodies: { [id: string]: CANNON.Body } = {}
-let meshId = 0
-
-const groundMirror = new Reflector(new THREE.PlaneGeometry(1024, 1024), {
-    color: new THREE.Color(0x222222),
-    clipBias: 0.003,
-    textureWidth: window.innerWidth * window.devicePixelRatio,
-    textureHeight: window.innerHeight * window.devicePixelRatio,
-})
-groundMirror.position.y = -0.05
-groundMirror.rotateX(-Math.PI / 2)
-scene.add(groundMirror)
-
-const planeShape = new CANNON.Plane()
-const planeBody = new CANNON.Body({ mass: 0 })
-planeBody.addShape(planeShape)
-planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
-world.addBody(planeBody)
-
-const convexObjectBreaker = new ConvexObjectBreaker()
-
-for (let i = 0; i < 20; i++) {
-    const size = {
-        x: Math.random() * 4 + 2,
-        y: Math.random() * 10 + 5,
-        z: Math.random() * 4 + 2,
-    }
-    const geo: THREE.BoxGeometry = new THREE.BoxGeometry(
-        size.x,
-        size.y,
-        size.z
+{
+    //create a cube and sphere and intersect them
+    const cubeMesh = new THREE.Mesh(
+        new THREE.BoxGeometry(2, 2, 2),
+        new THREE.MeshStandardMaterial({ color: 0xff0000 })
     )
-    const cube = new THREE.Mesh(geo, material)
-
-    cube.position.x = Math.random() * 50 - 25
-    cube.position.y = size.y / 2 + 0.1
-    cube.position.z = Math.random() * 50 - 25
-
-    scene.add(cube)
-    meshes[meshId] = cube
-    convexObjectBreaker.prepareBreakableObject(
-        meshes[meshId],
-        1,
-        new THREE.Vector3(),
-        new THREE.Vector3(),
-        true
+    const sphereMesh = new THREE.Mesh(
+        new THREE.SphereGeometry(1.45, 8, 8),
+        new THREE.MeshStandardMaterial({ color: 0x0000ff })
     )
+    cubeMesh.position.set(-5, 0, -6)
+    scene.add(cubeMesh)
+    sphereMesh.position.set(-2, 0, -6)
+    scene.add(sphereMesh)
 
-    const cubeShape = new CANNON.Box(
-        new CANNON.Vec3(size.x / 2, size.y / 2, size.z / 2)
+    const cubeCSG = CSG.fromMesh(cubeMesh, 0)
+    const sphereCSG = CSG.fromMesh(sphereMesh, 1)
+
+    const cubeSphereIntersectCSG = cubeCSG.intersect(sphereCSG)
+    const cubeSphereIntersectMesh = CSG.toMesh(
+        cubeSphereIntersectCSG,
+        new THREE.Matrix4(),
+        [cubeMesh.material, sphereMesh.material]
     )
-    const cubeBody = new CANNON.Body({ mass: 1 })
-    ;(cubeBody as any).userData = { splitCount: 0, id: meshId }
-    cubeBody.addShape(cubeShape)
-    cubeBody.position.x = cube.position.x
-    cubeBody.position.y = cube.position.y
-    cubeBody.position.z = cube.position.z
+    cubeSphereIntersectMesh.position.set(-2.5, 0, -3)
+    scene.add(cubeSphereIntersectMesh)
 
-    world.addBody(cubeBody)
-    bodies[meshId] = cubeBody
-
-    meshId++
-}
-
-const bullets: { [id: string]: THREE.Mesh } = {}
-const bulletBodies: { [id: string]: CANNON.Body } = {}
-let bulletId = 0
-
-const bulletMaterial = new THREE.MeshPhysicalMaterial({
-    map: new THREE.TextureLoader().load('img/marble.png'),
-    clearcoat: 1.0,
-    clearcoatRoughness: 0,
-    clearcoatMap: null,
-    clearcoatRoughnessMap: null,
-    metalness: 0.4,
-    roughness: 0.3,
-    color: 'white',
-})
-document.addEventListener('click', onClick, false)
-function onClick() {
-    if (controls.isLocked) {
-        const bullet = new THREE.Mesh(
-            new THREE.SphereGeometry(1, 16, 16),
-            bulletMaterial
-        )
-        bullet.position.copy(camera.position)
-        scene.add(bullet)
-        bullets[bulletId] = bullet
-
-        const bulletShape = new CANNON.Sphere(1)
-        const bulletBody = new CANNON.Body({ mass: 1 })
-        bulletBody.addShape(bulletShape)
-        bulletBody.position.x = camera.position.x
-        bulletBody.position.y = camera.position.y
-        bulletBody.position.z = camera.position.z
-
-        world.addBody(bulletBody)
-        bulletBodies[bulletId] = bulletBody
-
-        bulletBody.addEventListener('collide', (e: any) => {
-            if (e.body.userData) {
-                if (e.body.userData.splitCount < 2) {
-                    splitObject(e.body.userData, e.contact)
-                }
-            }
+    // create 3 cylinders and union them
+    const cylinderMesh1 = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.85, 0.85, 2, 8, 1, false),
+        new THREE.MeshStandardMaterial({
+            color: 0xffbf00,
         })
-        const v = new THREE.Vector3(0, 0, -1)
-        v.applyQuaternion(camera.quaternion)
-        v.multiplyScalar(50)
-        bulletBody.velocity.set(v.x, v.y, v.z)
-        bulletBody.angularVelocity.set(
-            Math.random() * 10 + 1,
-            Math.random() * 10 + 1,
-            Math.random() * 10 + 1
-        )
-
-        bulletId++
-
-        //remove old bullets
-        while (Object.keys(bullets).length > 5) {
-            scene.remove(bullets[bulletId - 6])
-            delete bullets[bulletId - 6]
-            world.removeBody(bulletBodies[bulletId - 6])
-            delete bulletBodies[bulletId - 6]
-        }
-    }
-}
-
-function splitObject(userData: any, contact: CANNON.ContactEquation) {
-    const contactId = userData.id
-    if (meshes[contactId]) {
-        const poi = bodies[contactId].pointToLocalFrame(
-            (contact.bj.position as CANNON.Vec3).vadd(contact.rj)
-        )
-        const n = new THREE.Vector3(
-            contact.ni.x,
-            contact.ni.y,
-            contact.ni.z
-        ).negate()
-        const shards = convexObjectBreaker.subdivideByImpact(
-            meshes[contactId],
-            new THREE.Vector3(poi.x, poi.y, poi.z),
-            n,
-            1,
-            0
-        )
-
-        scene.remove(meshes[contactId])
-        delete meshes[contactId]
-        world.removeBody(bodies[contactId])
-        delete bodies[contactId]
-
-        shards.forEach((d: THREE.Object3D) => {
-            const nextId = meshId++
-
-            scene.add(d)
-            meshes[nextId] = d as THREE.Mesh
-            ;(d as THREE.Mesh).geometry.scale(0.99, 0.99, 0.99)
-            const shape = gemoetryToShape((d as THREE.Mesh).geometry)
-
-            const body = new CANNON.Body({ mass: 1 })
-            body.addShape(shape)
-            ;(body as any).userData = {
-                splitCount: userData.splitCount + 1,
-                id: nextId,
-            }
-            body.position.x = d.position.x
-            body.position.y = d.position.y
-            body.position.z = d.position.z
-            body.quaternion.x = d.quaternion.x
-            body.quaternion.y = d.quaternion.y
-            body.quaternion.z = d.quaternion.z
-            body.quaternion.w = d.quaternion.w
-            world.addBody(body)
-            bodies[nextId] = body
+    )
+    const cylinderMesh2 = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.85, 0.85, 2, 8, 1, false),
+        new THREE.MeshStandardMaterial({
+            color: 0x00ff00,
         })
-    }
-}
+    )
+    const cylinderMesh3 = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.85, 0.85, 2, 8, 1, false),
+        new THREE.MeshStandardMaterial({
+            color: 0x9f2b68,
+        })
+    )
+    cylinderMesh1.position.set(1, 0, -6)
+    scene.add(cylinderMesh1)
+    cylinderMesh2.position.set(3, 0, -6)
+    cylinderMesh2.geometry.rotateX(Math.PI / 2)
+    scene.add(cylinderMesh2)
+    cylinderMesh3.position.set(5, 0, -6)
+    cylinderMesh3.geometry.rotateZ(Math.PI / 2)
+    scene.add(cylinderMesh3)
 
-function gemoetryToShape(geometry: THREE.BufferGeometry) {
-    const position = (geometry.attributes.position as THREE.BufferAttribute).array
-    const points: THREE.Vector3[] = []
-    for (let i = 0; i < position.length; i += 3) {
-        points.push(
-            new THREE.Vector3(position[i], position[i + 1], position[i + 2])
-        )
-    }
-    const convexHull = new ConvexGeometry(points)
-    const shape = CannonUtils.CreateConvexPolyhedron(convexHull)
-    return shape
+    const cylinderCSG1 = CSG.fromMesh(cylinderMesh1, 2)
+    const cylinderCSG2 = CSG.fromMesh(cylinderMesh2, 3)
+    const cylinderCSG3 = CSG.fromMesh(cylinderMesh3, 4)
+    const cylindersUnionCSG = cylinderCSG1.union(
+        cylinderCSG2.union(cylinderCSG3)
+    )
+
+    const cylindersUnionMesh = CSG.toMesh(
+        cylindersUnionCSG,
+        new THREE.Matrix4()
+    )
+    cylindersUnionMesh.material = [
+        cylinderMesh1.material,
+        cylinderMesh2.material,
+        cylinderMesh3.material,
+    ]
+    cylindersUnionMesh.position.set(2.5, 0, -3)
+    scene.add(cylindersUnionMesh)
+
+    //subtract the cylindersUnionCSG from the cubeSphereIntersectCSG
+    const finalCSG = cubeSphereIntersectCSG.subtract(cylindersUnionCSG)
+    const finalMesh = CSG.toMesh(finalCSG, new THREE.Matrix4())
+    finalMesh.material = [
+        cubeMesh.material,
+        sphereMesh.material,
+        cylinderMesh1.material,
+        cylinderMesh2.material,
+        cylinderMesh3.material,
+    ]
+    scene.add(finalMesh)
 }
 
 window.addEventListener('resize', onWindowResize, false)
@@ -301,51 +148,10 @@ function onWindowResize() {
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const options = {
-    side: {
-        FrontSide: THREE.FrontSide,
-        BackSide: THREE.BackSide,
-        DoubleSide: THREE.DoubleSide,
-    },
-}
-
-const clock = new THREE.Clock()
-let delta
-
 function animate() {
     requestAnimationFrame(animate)
 
-    delta = clock.getDelta()
-    if (delta > 0.1) delta = 0.1
-    world.step(delta)
-
-    Object.keys(meshes).forEach((m) => {
-        meshes[m].position.set(
-            bodies[m].position.x,
-            bodies[m].position.y,
-            bodies[m].position.z
-        )
-        meshes[m].quaternion.set(
-            bodies[m].quaternion.x,
-            bodies[m].quaternion.y,
-            bodies[m].quaternion.z,
-            bodies[m].quaternion.w
-        )
-    })
-
-    Object.keys(bullets).forEach((b) => {
-        bullets[b].position.set(
-            bulletBodies[b].position.x,
-            bulletBodies[b].position.y,
-            bulletBodies[b].position.z
-        )
-        bullets[b].quaternion.set(
-            bulletBodies[b].quaternion.x,
-            bulletBodies[b].quaternion.y,
-            bulletBodies[b].quaternion.z,
-            bulletBodies[b].quaternion.w
-        )
-    })
+    controls.update()
 
     render()
 
@@ -357,3 +163,4 @@ function render() {
 }
 
 animate()
+
