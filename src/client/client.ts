f068c3f02fe0933d35ad128ea6cc3b188183cb74
OmarMWarraich <earthling986@gmail.com>
2023-04-05 23:02:53 +0500

    feat: Raycaster Mouse Picking

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,21 +1,36 @@
 /* 
 * Description
-* While raycasting is almost always used for mouse picking objects in the 3D scene, it can also be used for simple collision detection.
-* In this example, I detect whether the orbit controls will penetrate another object and adjust the cameras position so that it stays outside.
-* Essentially, I am creating a ray from the camera target to the camera position. If there is an intersected object between, then the camera
-* position is adjusted to the intersect point. This prevents the camera from going behind a wall, or inside a box, or floor, or any object 
-* which is part of the objects array being tested for an intersect.
 * 
-*  Also, instead of using the raycaster to find the new point to position the camera in case of collision between the target and itself, 
-*  I could instead modify the opacity of the object in between and not move the camera. Rotate the camera and notice how any object between
-*  the camera target and the camera itself, becomes transparent.
+*  This is a basic example of using the Raycaster to mouse pick objects in the scene.
+*  The scene is traversed and all individual objects are added to the pickableObjects 
+*  array that is used by the Raycaster. The sphere and plane are deliberately excluded
+*  from this so they will not be mouse picked.
+*
+*  The plane also receives shadows while everything else only casts shadows.
+* 
+*  This example demonstrates,
+* 
+*  Loading a GLB scene, traversing the child objects to individually add and/or copy
+*  properties for later use.
+*  Using the Raycaster to detect if the mouse is over certain objects and changing
+*  there material.
+* 
 */
 
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
+
+const light = new THREE.SpotLight()
+light.position.set(12.5, 12.5, 12.5)
+light.castShadow = true
+light.shadow.mapSize.width = 1024
+light.shadow.mapSize.height = 1024
+scene.add(light)
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -23,102 +38,58 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 2
+camera.position.set(15, 15, 15)
 
 const renderer = new THREE.WebGLRenderer()
+renderer.shadowMap.enabled = true
+renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const raycaster = new THREE.Raycaster()
-const sceneMeshes: THREE.Mesh[] = []
-const dir = new THREE.Vector3()
-let intersects: THREE.Intersection[] = []
-
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
-controls.addEventListener('change', function () {
-    xLine.position.copy(controls.target)
-    yLine.position.copy(controls.target)
-    zLine.position.copy(controls.target)
 
-    raycaster.set(
-        controls.target,
-        dir.subVectors(camera.position, controls.target).normalize()
-    )
+const pickableObjects: THREE.Mesh[] = []
+let intersectedObject: THREE.Object3D | null
+const originalMaterials: { [id: string]: THREE.Material | THREE.Material[] } =
+    {}
+const highlightedMaterial = new THREE.MeshBasicMaterial({
+    wireframe: true,
+    color: 0x00ff00,
+})
 
-    intersects = raycaster.intersectObjects(sceneMeshes, false)
-    if (intersects.length > 0) {
-        if (
-            intersects[0].distance < controls.target.distanceTo(camera.position)
-        ) {
-            camera.position.copy(intersects[0].point)
-        }
+const loader = new GLTFLoader()
+loader.load(
+    'models/model9/simplescene.glb',
+    function (gltf) {
+        gltf.scene.traverse(function (child) {
+            if ((child as THREE.Mesh).isMesh) {
+                const m = child as THREE.Mesh
+                //the sphere and plane will not be mouse picked. THe plane will receive shadows while everything else casts shadows.
+                switch (m.name) {
+                    case 'Plane':
+                        m.receiveShadow = true
+                        break
+                    case 'Sphere':
+                        m.castShadow = true
+                        break
+                    default:
+                        m.castShadow = true
+                        pickableObjects.push(m)
+                        //store reference to original materials for later
+                        originalMaterials[m.name] = (m as THREE.Mesh).material
+                }
+            }
+        })
+        scene.add(gltf.scene)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
     }
-})
-
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshNormalMaterial({ side: THREE.DoubleSide })
 )
-floor.rotateX(-Math.PI / 2)
-floor.position.y = -1
-scene.add(floor)
-sceneMeshes.push(floor)
-
-const wall1 = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 2),
-    new THREE.MeshNormalMaterial({ side: THREE.DoubleSide })
-)
-wall1.position.x = 4
-wall1.rotateY(-Math.PI / 2)
-scene.add(wall1)
-sceneMeshes.push(wall1)
-
-const wall2 = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 2),
-    new THREE.MeshNormalMaterial({ side: THREE.DoubleSide })
-)
-wall2.position.z = -3
-scene.add(wall2)
-sceneMeshes.push(wall2)
-
-const cube: THREE.Mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(),
-    new THREE.MeshNormalMaterial()
-)
-cube.position.set(-3, 0, 0)
-scene.add(cube)
-sceneMeshes.push(cube)
-
-const ceiling = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshNormalMaterial({ side: THREE.DoubleSide })
-)
-ceiling.rotateX(Math.PI / 2)
-ceiling.position.y = 3
-scene.add(ceiling)
-sceneMeshes.push(ceiling)
-
-//crosshair
-const lineMaterial = new THREE.LineBasicMaterial({
-    color: 0x0000ff,
-})
-const points: THREE.Vector3[] = []
-points[0] = new THREE.Vector3(-0.1, 0, 0)
-points[1] = new THREE.Vector3(0.1, 0, 0)
-let lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-const xLine = new THREE.Line(lineGeometry, lineMaterial)
-scene.add(xLine)
-points[0] = new THREE.Vector3(0, -0.1, 0)
-points[1] = new THREE.Vector3(0, 0.1, 0)
-lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-const yLine = new THREE.Line(lineGeometry, lineMaterial)
-scene.add(yLine)
-points[0] = new THREE.Vector3(0, 0, -0.1)
-points[1] = new THREE.Vector3(0, 0, 0.1)
-lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-const zLine = new THREE.Line(lineGeometry, lineMaterial)
-scene.add(zLine)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -127,6 +98,34 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
+
+const raycaster = new THREE.Raycaster()
+let intersects: THREE.Intersection[]
+
+const mouse = new THREE.Vector2()
+
+function onDocumentMouseMove(event: MouseEvent) {
+    mouse.set(
+        (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+        -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+    )
+    raycaster.setFromCamera(mouse, camera)
+    intersects = raycaster.intersectObjects(pickableObjects, false)
+
+    if (intersects.length > 0) {
+        intersectedObject = intersects[0].object
+    } else {
+        intersectedObject = null
+    }
+    pickableObjects.forEach((o: THREE.Mesh, i) => {
+        if (intersectedObject && intersectedObject.name === o.name) {
+            pickableObjects[i].material = highlightedMaterial
+        } else {
+            pickableObjects[i].material = originalMaterials[o.name]
+        }
+    })
+}
+document.addEventListener('mousemove', onDocumentMouseMove, false)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
@@ -146,3 +145,4 @@ function render() {
 }
 
 animate()
+
