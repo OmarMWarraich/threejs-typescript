f30ab41781cddea2d6f949a69d5cc7833c7a6cb4
OmarMWarraich <earthling986@gmail.com>
2023-04-06 11:45:16 +0500

    feat: Download Progress Indicator Part-1

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,20 +1,22 @@
 /* 
 *  Description
 *
-*  Collision Detection using OBB: Oriented Bounding Box
+*  Download Progress Indicator
 *
 */
 
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
-import { OBB } from 'three/examples/jsm/math/OBB'
 
 const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.AmbientLight()
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(1, 1, 1)
 scene.add(light)
+
+scene.add(new THREE.AmbientLight(0xffffff, 0.25))
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -22,7 +24,7 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.set(0.8, 1.4, 3.0)
+camera.position.set(1.15, 1.15, 1.15)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -30,69 +32,40 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
+controls.target.y = 1
 
-const geometry = new THREE.BoxGeometry(1, 2, 3)
-geometry.computeBoundingBox()
-const material = new THREE.MeshPhongMaterial()
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(4, 1, 0)
-mesh.geometry.userData.obb = new OBB().fromBox3(
-    mesh.geometry.boundingBox as THREE.Box3
+const progressBar = document.getElementById(
+    'progressBar'
+) as HTMLProgressElement
+
+const loader = new GLTFLoader()
+loader.load(
+    'models/model10/xbot.glb',
+    function (gltf) {
+        progressBar.style.display = 'none'
+        scene.add(gltf.scene)
+    },
+    (xhr) => {
+        const percentComplete = (xhr.loaded / xhr.total) * 100
+        progressBar.value = percentComplete === Infinity ? 100 : percentComplete
+    }
 )
-mesh.userData.obb = new OBB()
-scene.add(mesh)
 
-const mesh2 = new THREE.Mesh(
-    geometry,
-    new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
-)
-mesh2.position.set(-3, 1, 0)
-mesh2.geometry.userData.obb = new OBB().fromBox3(
-    mesh2.geometry.boundingBox as THREE.Box3
-)
-mesh2.userData.obb = new OBB()
-
-scene.add(mesh2)
-
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20, 20, 10, 10),
-    new THREE.MeshBasicMaterial({ color: 0xaec6cf, wireframe: true })
-)
-floor.rotateX(-Math.PI / 2)
-scene.add(floor)
-
-window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
+window.addEventListener('resize', onWindowResize, false)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const clock = new THREE.Clock()
-
 function animate() {
     requestAnimationFrame(animate)
 
-    mesh.position.x = Math.sin(clock.getElapsedTime() * 0.5) * 4
-
     controls.update()
-
-    mesh.userData.obb.copy(mesh.geometry.userData.obb)
-    mesh2.userData.obb.copy(mesh2.geometry.userData.obb)
-    mesh.userData.obb.applyMatrix4(mesh.matrixWorld)
-    mesh2.userData.obb.applyMatrix4(mesh2.matrixWorld)
-    if (mesh.userData.obb.intersectsOBB(mesh2.userData.obb)) {
-        mesh.material.color.set(0xff0000)
-    } else {
-        mesh.material.color.set(0x00ff00)
-    }
-
-    mesh.rotateY(0.01)
-    mesh2.rotateY(-0.005)
 
     render()
 
@@ -104,5 +77,6 @@ function render() {
 }
 
 animate()
+
 
 
